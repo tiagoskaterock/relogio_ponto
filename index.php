08fe0f3fbc931a83a2bddbdcb65d64ef99ab8e4d
Tiago Lemes <tiago.lemes@windel.com.br>
2022-12-26 10:01:04 -0300

    252. Tela de Autenticação

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $env_path = realpath(dirname(__FILE__, 1)) . "/src/config/env.ini";
 $env = parse_ini_file($env_path);
 
 require_once(dirname(__FILE__) . "/src/config/config.php");
-require_once(dirname(__FILE__) . "/src/models/User.php");
+require_once(dirname(__FILE__) . "/src/views/login.php");
 
 Database::getConnection();
 
@@ -22,19 +22,19 @@ Database::getConnection();
 // $user->email = 'novo@email.com';
 // print_r($user->email);
 
-print_r(User::get(['id' => 2], 'name, email'));
+// print_r(User::get(['id' => 2], 'name, email'));
 
-print_r(User::get([], 'name'));
+// print_r(User::get([], 'name'));
 
 // echo '<br>';
 
 // echo User::getSelect(['name' => 'Chaves']);
 
-foreach (User::get() as $user) {
-	echo "<pre>";
-	print_r($user);
-	echo "</pre>";
-}
+// foreach (User::get() as $user) {
+// 	echo "<pre>";
+// 	print_r($user);
+// 	echo "</pre>";
+// }
 
 
 
---
+++ src/views/login.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>In n' Out</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/comum.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/icofont.min.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/login.css">
+</head>
+<body>
+
+	<form action="#" method="post" class="form-login">
+		<div class="card login-card">
+			<div class="card-header">
+				<i class="icofont-travelling mr-2"></i>
+				<span class="font-weight-light">In</span>
+				<span class="font-bold-light mx-2">N'</span>
+				<span class="font-weight-light">Out</span>
+				<i class="icofont-runner-alt-1 ml-2"></i>
+			</div>
+
+			<div class="card-body">
+				<div class="form-group">
+					<label for="email">Email</label>
+					<input class="form-control" type="email" name="email" placeholder="Informe o email">
+				</div>
+
+				<div class="form-group">
+					<label for="password">Password</label>
+					<input class="form-control" type="password" name="password" placeholder="Informe a senha">
+				</div>
+
+			</div>
+
+			<div class="card-footer">
+				<button class="btn btn-lg btn-primary">Entrar</button>
+			</div>	
+		</div>
+	</form>
+
+</body>
+</html>
